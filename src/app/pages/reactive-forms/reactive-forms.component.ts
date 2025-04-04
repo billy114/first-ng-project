a3e564639ffe840413ba3c46f3a79dcddb0fdffe
Billel <billel.abbes@hotmail.com>
2025-04-04 11:07:02 +0200

    form group

--- src/app/pages/reactive-forms/reactive-forms.component.ts
+++ src/app/pages/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {NgClass} from '@angular/common';
 
 @Component({
@@ -13,22 +13,48 @@ import {NgClass} from '@angular/common';
   styleUrl: './reactive-forms.component.scss'
 })
 export class ReactiveFormsComponent {
-  name: FormControl = new FormControl('', [Validators.required]);
-  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  formTest : FormGroup = new FormGroup({
+    name : new FormControl('', [Validators.required]),
+    email : new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('',  [
+      Validators.required,
+      Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$")
+    ]),
+    confPassword: new FormControl('', [Validators.required]),
+    languages : new FormArray<FormControl<string | null>>([
+      new FormControl('', Validators.required)
+    ])
+  })
 
-  password: FormControl = new FormControl('', [
-    Validators.required,
-    Validators.pattern("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{8,}$")
-  ]);
-  confPassword: FormControl = new FormControl('', [Validators.required]);
+  get languages() : FormArray<FormControl<string | null>>{
+    return this.formTest.controls['languages'] as FormArray<FormControl<string | null>>;
+  }
 
+  get email() : FormControl {
+    return this.formTest.controls['email'] as FormControl;
+  }
 
+  get password() : FormControl {
+    return this.formTest.controls['password'] as FormControl;
+  }
 
-  getNameValue(){
-    return this.name.value;
+  get confPassword() : FormControl {
+    return this.formTest.controls['confPassword'] as FormControl;
   }
 
   isMatched() : boolean{
     return this.password.value === this.confPassword.value;
+  }
+
+  addLanguageInput() : void{
+    this.languages.controls.push(new FormControl('', Validators.required));
+  }
+
+  removeLanguageInput(index : number){
+      this.languages.controls.splice(index, 1);
+  }
+
+  onSubmit(){
+    console.log(this.formTest.value);
   }
 }
